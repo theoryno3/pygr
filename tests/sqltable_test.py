ae123450b3238c7b0c35cb1d08772f692328f8c7
theoryno3 <pryce@ucla.edu>
2009-09-01 19:30:30 -0700

    tests/sqltable_test.py:  -Using mysql as default database for DBServerInfo and sqlite for GenericServerInfo  -python2.3 => 8/69 tests fail. specifically, the 3rd 'test changing an attr value'.   below are the failed tests followed by the exact error message:
      this is the 3rd table creation (at least, the third 'test changing an attr value')
      test changing an attr value ... ERROR
      test row creation with auto inc ID ... ERROR
      check row creation with specified ID ... ERROR
      test assigning new ID to existing object ... ERROR
      test changing an attr value ... ERROR
      test row creation with auto inc ID ... ERROR
      check row creation with specified ID ... ERROR
      test assigning new ID to existing object ... ERROR
    
    ======================================================================
    ERROR: test changing an attr value
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "tests/sqltable_test.py", line 223, in test_attr
            serverInfo=self.serverInfo) # requery the db
      File "/home/prigor/projects/github/pygr.upstream/pygr/sqlgraph.py", line 518, in __init__
            self.get_table_schema() # get schema of columns to serve as attrs
      File "/home/prigor/projects/github/pygr.upstream/pygr/sqlgraph.py", line 449, in get_table_schema
            return schema_func(self, analyzeSchema)
      File "/home/prigor/projects/github/pygr.upstream/pygr/sqlgraph.py", line 2004, in get_table_schema
            return sqlite_table_schema(db, analyzeSchema)
      File "/home/prigor/projects/github/pygr.upstream/pygr/sqlgraph.py", line 338, in sqlite_table_schema
            self.cursor.execute('PRAGMA table_info("%s")' % self.name)
      InterfaceError: Cursor needed to be reset because of commit/rollback and can no longer be fetched from.

--- tests/sqltable_test.py
+++ tests/sqltable_test.py
@@ -12,8 +12,9 @@ class SQLTable_Setup(unittest.TestCase):
         unittest.TestCase.__init__(self, *args, **kwargs)
         if sqlalchemy_compatible(silent_fail=True):
             self.serverInfo = GenericServerInfo("sqlite:///test.sqlite.db") # sqlalchemy
+            #self.serverInfo = GenericServerInfo("mysql://test@localhost/test_pygr") # sqlalchemy
         else:
-            self.serverInfo =  DBServerInfo() # share conn for all tests, non-sqlalchemy
+            self.serverInfo =  DBServerInfo(host="localhost",user="test",db="test_pygr") # share conn for all tests, non-sqlalchemy
     def setUp(self):
         try:
             self.load_data(writeable=self.writeable)
